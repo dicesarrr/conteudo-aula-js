78fefa3fa2fc758e02eae174b7f4b100f6cb0caf
Richard Cesar <richardcesar911@gmail.com>
2026-02-24 22:11:13 -0300

    Aula 1 - Variáveis em JavaScript

---
+++ Aula1/src/script.js
@@ -0,0 +1,11 @@
+// Exemplos de variável let
+let nome = 'Richard';
+let nome2 = 'Nathalia';
+
+console.log(nome);
+console.log(nome2);
+
+// Exemplos de varíavel const
+const times = ['São Paulo', 'Corinthians', 'Santos'];
+
+console.log(times);
