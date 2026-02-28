8c2963cc5a1789685e3339ccfcbab231146dad21
Richard Cesar <richardcesar911@gmail.com>
2026-02-27 22:12:11 -0300

    Aula 4 - Condições Lógicas(Else, Switch e Ternário)

---
+++ Aula4/src/script02.js
@@ -0,0 +1,34 @@
+// Exemplo de If Else
+// const hora = new Date().getHours();
+// const hora = 18;
+// const minutos = new Date().getMinutes();
+// const horario = String(hora) + ':' + String(minutos);
+// let saudacao; 
+
+// if (hora > 18) {
+//   saudacao = 'Boa noite!';
+// } else {
+//   saudacao = 'Bom dia!';
+// }
+
+// document.getElementById('ex1').innerHTML = saudacao;
+// // document.getElementById('ex2').innerHTML = 'Agora são: ' + horario;
+// document.getElementById('ex2').innerHTML = `Agora são: ${horario}`;
+
+// Exemplo If Else If
+const hora = 8;
+const minutos = new Date().getMinutes();
+const horario = String(hora) + ':' + String(minutos);
+let saudacao; 
+
+if (hora > 18) {
+  saudacao = 'Boa noite!';
+} else if (hora > 12) {
+  saudacao = 'Boa tarde!';
+} else {
+  saudacao = 'Bom dia!';
+}
+
+document.getElementById('ex1').innerHTML = saudacao;
+// document.getElementById('ex2').innerHTML = 'Agora são: ' + horario;
+document.getElementById('ex2').innerHTML = `Agora são: ${horario}`;
